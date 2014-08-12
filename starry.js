62f54ac929d057cf6580a1eb9205cffa85dde570
Andre Sieverding <webmaster@andre-sieverding.de>
2014-08-12 18:44:04 +0200

    Update starry.js

--- starry.js
+++ starry.js
@@ -286,11 +286,17 @@ function Starry (element) {
 	Starry.prototype.setRating = function (rating) {
 		if (this.stars === true) {
 			this.destroy();
+			
+			var firstSettings;
+			var settings;
 
+			firstSettings = this.initSettings;
 			settings = this.initSettings;
 			settings.startValue = rating;
 
 			this.init(settings);
+			
+			this.initSettings = firstSettings;
 		}
 	}
 
